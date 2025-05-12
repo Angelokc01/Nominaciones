c13b4d7424614a09276f2486171c8ef07b2e97c0
Angelokc01 <gamadavidjuanluis.ag@gmail.com>
2025-05-12 00:35:06 -0500

    Sin app Nominaciones

--- Nominaciones/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import Nominaciones
-
-admin.site.register(Nominaciones)
-
--- Nominaciones/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NominacionesConfig(AppConfig):
-    name = 'Nominaciones'
